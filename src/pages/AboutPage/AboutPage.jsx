6801f76c640ed67b30deb2da968031355c4644be
Héctor Coronado <hm.covar@gmail.com>
2023-07-03 23:38:23 -0500

    ojala no se rompa ><

---
+++ src/pages/AboutPage/AboutPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1 style={{ display: "flex", justifyContent: "center" }}>AboutPage</h1>
+    </div>
+  );
+};
+
+export default AboutPage;
